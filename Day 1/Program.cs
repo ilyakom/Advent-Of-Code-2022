54c6ad60b974bae341fb5ba35608ec0b68573944
ikomlyakov <ikomlyakov@MS_ILYA>
2022-12-02 11:32:01 +0100

    Add project files.

---
+++ Day 1/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var list = new List<int>();
+
+        using var reader = File.OpenRead("./input.txt");
+        using var s = new StreamReader(reader);
+
+        var localSum = 0;
+        var line = string.Empty;
+
+        while (!s.EndOfStream)
+        {
+            line = s.ReadLine();
+
+            if (string.IsNullOrEmpty(line))
+            {
+                list.Add(localSum);
+                localSum = 0;
+            }
+            else
+            {
+                localSum += int.Parse(line);
+            }
+        }
+
+        Console.WriteLine(list.OrderByDescending(x => x).Take(3).Sum());
+    }
+}
---
+++ Day 2/Program.cs
@@ -0,0 +1,46 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var list = new List<int>();
+
+        using var reader = File.OpenRead("./input.txt");
+        using var s = new StreamReader(reader);
+
+        var score = 0;
+
+        while (!s.EndOfStream)
+        {
+            var line = s.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            if (line[1] == "Y" && line[0] == "A" || 
+                line[1] == "Z" && line[0] == "C" || 
+                line[1] == "X" && line[0] == "B")
+            {
+                score += 1;
+            }
+            else if (line[1] == "Y" && line[0] == "B" ||
+                line[1] == "Z" && line[0] == "A" ||
+                line[1] == "X" && line[0] == "C")
+            {
+                score += 2;
+            }
+            else
+            {
+                score += 3;
+            }
+
+            if (line[1] == "Y" )
+            {
+                score+= 3;
+            }
+            else if (line[1] == "Z")
+            {
+                score += 6;
+            }
+
+        }
+
+        Console.WriteLine(score);
+    }
+}
