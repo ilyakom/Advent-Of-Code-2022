35fc1a912bb8e45222c869047233edd40da79612
Ilya Komlyakov <ikomlyakov@microsoft.com>
2022-12-10 17:09:50 +0100

    Day 9 + 10. Gave up to commit it yesterday. Because of woman ^_^

---
+++ Day 10/Program.cs
@@ -0,0 +1,58 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        using var reader = File.OpenRead("./input.txt");
+        using var s = new StreamReader(reader);
+
+        var cycle = 0;
+        var register = 1;
+
+        while (!s.EndOfStream)
+        {
+            var line = s.ReadLine();
+
+            if (line == "noop")
+            {
+                Draw(cycle, register);
+                cycle++;
+                NewLine(ref cycle);
+            }
+            else
+            {
+                var num = int.Parse(line.Split(" ").Last());
+
+                Draw(cycle, register);
+                cycle++;
+                NewLine(ref cycle);
+
+                Draw(cycle, register);
+                cycle++;
+                NewLine(ref cycle);
+
+                register += num;
+            }
+        }
+    }
+
+    private static void NewLine(ref int cycle)
+    {
+        if (cycle == 40)
+        {
+            cycle = 0;
+            Console.WriteLine();
+        }
+    }
+
+    private static void Draw(int cycle, int register)
+    {
+        if (cycle >= register - 1 && cycle <= register + 1)
+        {
+            Console.Write('#');
+        }
+        else
+        {
+            Console.Write('.');
+        }
+    }
+}
---
+++ Day 9/Program.cs
@@ -0,0 +1,107 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        using var reader = File.OpenRead("./input.txt");
+        using var s = new StreamReader(reader);
+
+        var snake = new Point[10];
+
+        for (int i = 0;i < snake.Length; i++)
+        {
+            snake[i] = new Point();
+        }
+
+        var hs = new HashSet<Point> { new Point { x = 0, y = 0 } };
+
+        while (!s.EndOfStream)
+        {
+            var line = s.ReadLine().Split(' ');
+
+            var dir = line[0];
+            var cnt = int.Parse(line[1]);
+
+            for (int i = 0; i < cnt; i++)
+            {
+                switch (dir)
+                {
+                    case "R":
+                        snake[0].x++;
+                        break;
+                    case "L":
+                        snake[0].x--;
+                        break;
+                    case "U":
+                        snake[0].y++;
+                        break;
+                    case "D":
+                        snake[0].y--;
+                        break;
+                    default:
+                        break;
+                }
+
+                Move(snake);
+
+                hs.Add(snake[snake.Length-1]);
+            }
+        }
+
+        Console.WriteLine(hs.Count);
+    }
+
+    private static void Move(Point[] snake)
+    {
+        for (int i = 1; i < snake.Length; i++)
+        {
+            var isXMove = Math.Abs(snake[i - 1].x - snake[i].x) > 1;
+            var isYMove = Math.Abs(snake[i - 1].y - snake[i].y) > 1;
+
+            if (isXMove && isYMove)
+            {
+                snake[i].x = snake[i - 1].x - Math.Sign(snake[i - 1].x - snake[i].x);
+                snake[i].y = snake[i - 1].y - Math.Sign(snake[i - 1].y - snake[i].y);
+            }
+            else if (isXMove)
+            {
+                snake[i].x = snake[i-1].x - Math.Sign(snake[i-1].x - snake[i].x);
+
+                if (snake[i-1].y != snake[i].y)
+                {
+                    snake[i].y = snake[i-1].y;
+                }
+            }
+            else if (isYMove)
+            {
+                snake[i].y = snake[i-1].y - Math.Sign(snake[i-1].y - snake[i].y);
+
+                if (snake[i-1].x != snake[i].x)
+                {
+                    snake[i].x = snake[i-1].x;
+                }
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+
+    class Point
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(x, y);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            var other = obj as Point;
+
+            return this.x == other.x && this.y == other.y;
+        }
+    }
+}
