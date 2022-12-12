2b147d159dcf4f147b21643a49049b044814ae71
Ilya Komlyakov <ikomlyakov@microsoft.com>
2022-12-12 23:13:01 +0100

    Day 11 and 12. Those two days were really interesting!

---
+++ Day 11/Program.cs
@@ -0,0 +1,139 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        using var reader = File.OpenRead("./input.txt");
+        using var s = new StreamReader(reader);
+
+        var monkeys = new List<Monkey>();
+        var mon = new Monkey { AllMonkeys = monkeys };
+
+        while (!s.EndOfStream)
+        {
+            var line = s.ReadLine().Trim();
+
+            if (string.IsNullOrEmpty(line))
+            {
+                monkeys.Add(mon);
+                mon = new Monkey
+                {
+                    AllMonkeys = monkeys
+                };
+            }
+            else if (line.StartsWith("Starting items"))
+            {
+                foreach (var item in line.Split(':')[1].Split(',').Select(x => long.Parse(x.Trim())))
+                {
+                    mon.Items.Enqueue(item);
+                }
+            }
+            else if (line.StartsWith("Monkey"))
+            {
+                mon.Id = int.Parse(line.Split(new char[2] {' ', ':'}, StringSplitOptions.RemoveEmptyEntries)[1]);
+            }
+            else if (line.StartsWith("Test"))
+            {
+                mon.Dividor = long.Parse(line.Split(' ').Last());
+            }
+            else if (line.StartsWith("If true"))
+            {
+                mon.TrueTo = int.Parse(line.Split(' ').Last());
+            }
+            else if (line.StartsWith("If false"))
+            {
+                mon.FalseTo = int.Parse(line.Split(' ').Last());
+            }
+        }
+
+        monkeys.Add(mon);
+
+        long preventor = 1;
+
+        foreach (var monkey in monkeys)
+        {
+            preventor *= monkey.Dividor;
+        }
+
+        for (long i = 1; i <= 10000; i++)
+        {
+            foreach (var monkey in monkeys)
+            {
+                while (monkey.Items.Count > 0)
+                {
+                    monkey.InspectNext(preventor);
+                }
+            }
+        }
+
+        var res = monkeys.OrderByDescending(x => x.InsCount).ToArray();
+
+        Console.WriteLine(res[0].InsCount * res[1].InsCount);
+    }
+}
+
+public class Monkey
+{
+    public int Id { get; set; }
+
+    public Queue<long> Items { get; set; }
+
+    public long Dividor { get; set; }
+
+    public int TrueTo { get; set; }
+
+    public int FalseTo { get; set; }
+
+    public long InsCount { get; set; }
+
+    public List<Monkey> AllMonkeys { get; set; }
+
+    public Func<long, long> Operation { 
+        get 
+        {
+            switch (Id)
+            {
+                case 0:
+                    return (long x) => checked(x * 19);
+                case 1:    
+                    return (long x) => checked(x + 1);
+                case 2:                
+                    return (long x) => checked(x + 8);
+                case 3:                
+                    return (long x) => checked(x * x);
+                case 4:                
+                    return (long x) => checked(x + 6);
+                case 5:                
+                    return (long x) => checked(x * 17);
+                case 6:                
+                    return (long x) => checked(x + 5);
+                case 7:                
+                    return (long x) => checked(x + 3);
+                default:
+                    throw new Exception();
+            }
+        }}
+
+    public Monkey()
+    {
+        Items = new Queue<long>();
+    }
+
+    public void InspectNext(long preventor)
+    {
+        InsCount++;
+
+        var item = Items.Dequeue();
+        item = Operation(item) % preventor;
+
+        if (item % Dividor == 0)
+        {
+            AllMonkeys[TrueTo].Items.Enqueue(item);
+        }
+        else
+        {
+            AllMonkeys[FalseTo].Items.Enqueue(item);
+        }
+    }
+}
---
+++ Day 12/Program.cs
@@ -0,0 +1,97 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        using var reader = File.OpenRead("./input.txt");
+        using var s = new StreamReader(reader);
+
+        
+        var map = new char[41, 159];
+        var w = new int[41, 159];
+        var q = new Queue<int[]>();
+
+        int rc = 0;
+        var end = new int[2];
+
+        while (!s.EndOfStream)
+        {
+            var line = s.ReadLine();
+
+            for (int i = 0; i < line.Length; i++)
+            {
+                map[rc, i] = line[i];
+                w[rc, i] = int.MaxValue;
+
+                if (line[i] == 'S')
+                {
+                    q.Enqueue(new[] { rc, i });
+                    w[rc, i] = 0;
+                    map[rc, i] = 'a';
+                }
+
+                if (line[i] == 'E')
+                {
+                    end[0] = rc;
+                    end[1] = i;
+                    map[rc, i] = 'z';
+                }
+
+                if (line[i] == 'a')
+                {
+                    q.Enqueue(new[] { rc, i });
+                    w[rc, i] = 0;
+                }
+            }
+
+            rc++;
+        }
+
+        while (q.Count > 0)
+        {
+            var cur = q.Dequeue();
+
+            if (cur[0] == end[0] && cur[1] == end[1])
+            {
+                continue;
+            }
+            
+            if (cur[0] != 0 && 
+                map[cur[0], cur[1]] - map[cur[0]-1, cur[1]] >= -1 &&
+                w[cur[0] - 1, cur[1]] > w[cur[0], cur[1]] + 1)
+            {
+                w[cur[0] - 1, cur[1]] = w[cur[0], cur[1]] + 1;
+
+                q.Enqueue(new [] { cur[0] - 1, cur[1] });
+            }
+
+            if (cur[0] != 40 &&
+                map[cur[0], cur[1]] - map[cur[0] + 1, cur[1]] >= -1 &&
+                w[cur[0] + 1, cur[1]] > w[cur[0], cur[1]] + 1)
+            {
+                w[cur[0] + 1, cur[1]] = w[cur[0], cur[1]] + 1;
+
+                q.Enqueue(new[] { cur[0] + 1, cur[1] });
+            }
+
+            if (cur[1] != 0 &&
+                map[cur[0], cur[1]] - map[cur[0], cur[1] - 1] >= -1 &&
+                w[cur[0], cur[1] - 1] > w[cur[0], cur[1]] + 1)
+            {
+                w[cur[0], cur[1] - 1] = w[cur[0], cur[1]] + 1;
+
+                q.Enqueue(new[] { cur[0], cur[1] - 1 });
+            }
+
+            if (cur[1] != 158 &&
+                map[cur[0], cur[1]] - map[cur[0], cur[1] + 1] >= -1 &&
+                w[cur[0], cur[1] + 1] > w[cur[0], cur[1]] + 1)
+            {
+                w[cur[0], cur[1] + 1] = w[cur[0], cur[1]] + 1;
+
+                q.Enqueue(new[] { cur[0], cur[1] + 1 });
+            }
+        }
+
+        Console.WriteLine(w[end[0], end[1]]);
+    }
+ }
